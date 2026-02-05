3c4790b2e388599f4e76c6a0d18b83fa12ffa1dd
Sanjay Birla <birlasanjay555@gmail.com>
2026-02-05 12:14:22 +0530

    created user schema and connect db

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,15 @@
-
+import connectDB from '../src/config/db.js'
 import express from 'express'
-
 const app=express();
+
+app.use(express.json());// for res.body
+// app.use('/auth/api',authRoutes)
+
+connectDB();
+
+app.get('/',(req,res)=>{
+    console.log("Server strated")
+    res.send("Hello")
+})
 
 export default app;
